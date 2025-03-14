0d06097ec784a211640208b12b845e631cd220c5
ramji <ramjib2311@gmail.com>
2025-03-14 15:34:14 +0530

    * origin

--- server.js
+++ server.js
@@ -21,33 +21,13 @@ const secret = process.env.JWT_SECRET;
 
 
 
-app.use(cors({ origin: 'https://srm-corrections-hy91.vercel.app' }));
+app.use(cors());
 // Updated CORS configuration to allow the frontend domain
-const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:3000',
-  'http://localhost:5000',
-  'https://srm-corrections-hy91.vercel.app',
-  'https://societycis.org'
-];
 
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      return callback(null, true);
-    }
-    console.log("Blocked CORS request from:", origin);
-    const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-    return callback(new Error(msg), false);
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
 
 app.use(express.json());
 
-app.options('*', cors());
+
 
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
